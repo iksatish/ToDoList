2d36e728035070a091bedb6038c59597348fab20
Satish <iksatish.k@gmail.com>
2016-01-30 06:43:21 -0800

    Initial Commit

---
+++ TaskList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskList
+//
+//  Created by Satish Kumar R Kancherla on 1/30/16.
+//  Copyright © 2016 Satish Kumar R Kancherla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaskList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaskList
+//
+//  Created by Satish Kumar R Kancherla on 1/30/16.
+//  Copyright © 2016 Satish Kumar R Kancherla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
